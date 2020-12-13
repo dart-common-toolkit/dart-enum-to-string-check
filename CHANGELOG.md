f440d42ff35baccedb8c6d38f6bf1e843e267754
fartem <jaman.smlnsk@gmail.com>
2020-12-13 16:13:19 +0300

    2020-12-13 Version 0.6.2: Fixed plugin imports

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,8 @@
 
 ## 0.6.1
 
+* fixed plugin imports.
+
+## 0.6.1
+
 * adopted to `pub.dev` publishing.
--- tools/analyzer_plugin/bin/plugin.dart
+++ tools/analyzer_plugin/bin/plugin.dart
@@ -1,6 +1,6 @@
 import 'dart:isolate';
 
-import 'package:dart_enum_to_string_check/analyzer_plugin.dart';
+import 'package:dart_enum_to_string_check/dart_enum_to_string_check.dart';
 
 void main(List<String> args, SendPort sendPort) {
   start(args, sendPort);
